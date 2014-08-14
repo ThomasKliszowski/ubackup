6896b8531f03ff3ba03536e686fb33706625c923
Thomas Kliszowski <thomas@stupeflix.com>
2014-08-14 17:31:26 +0200

    add RotatingFileHandler to logging

--- ubackup/settings.py
+++ ubackup/settings.py
@@ -36,6 +36,25 @@ LOGGING = {
     },
 }
 
+# Add a RotatingFileHandler to logging
+if not os.environ.get('TESTING'):
+    if not os.path.exists('/var/log/ubackup'):
+        try:
+            os.mkdir('/var/log/ubackup')
+        except OSError:
+            pass
+    if os.path.exists('/var/log/ubackup'):
+        LOGGING['handlers']['rotate_file'] = {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': '/var/log/ubackup/ubackup.log',
+            'backupCount': 5,
+            'maxBytes': 1024 * 1024 * 20
+        }
+        for logger, obj in LOGGING['loggers'].items():
+            obj['handlers'].append('rotate_file')
+
 DROPBOX_APP_KEY = 'rqqumcq9htn0fcb'
 DROPBOX_APP_SECRET = 'ag8dvti5kx2cfg9'
 
